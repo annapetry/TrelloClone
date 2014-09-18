fc20a325ff85b9258b2535935960bc7a95574323
Anna Petry <hello@annapetry.com>
2014-09-17 21:32:34 -0700

    Intial Commit

---
+++ app/assets/javascripts/trello_clone.js
@@ -0,0 +1,11 @@
+window.TrelloClone = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    // create collection ivars
+    // instantiate router & pass in DOM el
+    // ?
+  }
+};
